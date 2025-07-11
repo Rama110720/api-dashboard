bd49b4a8a6f692871a0e5fd755a5f96a14dfc78e
Rama110720 <ramahdr782@gmail.com>
2025-07-11 14:56:47 +0700

    Update api-manager.js

--- api/api-manager.js
+++ api/api-manager.js
@@ -37,6 +37,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-API-Key')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
 
   const method = req.method
   let apis = readAPIs()
